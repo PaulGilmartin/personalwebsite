6ce16541901aeab874b2e3db4a3e1ca99b0bd0a9
Paul Gilmartin <paulgilmartin@Paul-Gilmartins-MacBook-Pro.local>
2018-10-18 22:23:10 +0100

    added paintings to db

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -31,8 +31,26 @@ def get_all_items_of_type(model_name, config_class=Config):
     return all_items
 
 
+def delete_item(item, database=None, config_class=Config):
+    with app_context(config_class):
+        database = database or db
+        database.session.delete(item)
+        database.session.commit()
 
 
+def build_records(database=None, config_class=Config):
+    with app_context(config_class):
+        database = database or db
 
+        kgPhoto = create_model_item('Photograph', content_path='../static/kelvinbridgePhotograph.JPG',
+                                    title='Kelvinbridge', subject='Glasgow')
+        kgPainting = create_model_item('Painting', content_path='../static/kelvinBridgePainting.png',
+                                       title='Kelvinbridge', subject='Glasgow', photograph=kgPhoto)
 
+        wPhoto = create_model_item('Photograph', content_path='../static/westPrincesStreetPhotograph.JPG',
+                                   title='West Princes Street', subject='Glasgow')
+        wPainting = create_model_item('Painting', content_path='../static/westPrincesStreetPainting.png',
+                                      title='West Princes Street', subject='Glasgow', photograph=wPhoto)
 
+        for i in [kgPhoto, kgPainting, wPhoto, wPainting]:
+            commit_item(i)
